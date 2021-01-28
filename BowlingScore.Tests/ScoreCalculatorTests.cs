6f41931b047962f16b5acba0dcd93aef8e90b1ba
Tudor Ioneasa <t.ioneasa@levi9.com>
2021-01-28 16:09:00 +0200

    gitignore fix

--- BowlingScore.Tests/ScoreCalculatorTests.cs
+++ BowlingScore.Tests/ScoreCalculatorTests.cs
@@ -11,8 +11,23 @@ namespace BowlingScore.Tests
         [Fact]
         public void CalculateScore_TotalScore_ReturnSuccess()
         {
-            var expectedTotal = 150;
+            var expectedTotal = 90;
             var inputFilePath = @"./TestInput.txt";
+            InputReader reader = new InputReader();
+            ScoreCalculator calculator = new ScoreCalculator(reader, inputFilePath);
+
+            //act
+            var total = calculator.CalculateScore();
+
+            //assert
+            Assert.Equal(expectedTotal, total);
+        }
+
+        [Fact]
+        public void CalculateScore_EdgeCaseTotalScore_ReturnSuccess()
+        {
+            var expectedTotal = 199;
+            var inputFilePath = @"./TestInputEdgeCase.txt";
             InputReader reader = new InputReader();
             ScoreCalculator calculator = new ScoreCalculator(reader, inputFilePath);
 
--- BowlingScore/ScoreCalculator.cs
+++ BowlingScore/ScoreCalculator.cs
@@ -12,9 +12,10 @@ namespace BowlingScore
         public int TotalScore { get; set; }
         public List<int> FrameScoreList { get; set; }
         public string[] ThrowArray { get; set; }
+        public int[] GroupedThrowsArray { get; set; }
 
         const int MaxScore = 10;
-        public ScoreCalculator(InputReader inputReader, string filePath)
+        public ScoreCalculator(InputReader inputReader, string filePath) // dependency injection needs interface to use for mock testing
         {
             _filePath = filePath;
             _inputReader = inputReader;
@@ -23,63 +24,43 @@ namespace BowlingScore
 
         public int CalculateScore()
         {
-            var newArr = CreateNewArray(ThrowArray);
+            GroupedThrowsArray = ExtractScores(ThrowArray); // should I use this or use var GroupedThrowsArrayay = ExtractScores(,,)
             Console.Write("New array: [ ");
-            foreach (var throwValue in newArr)
+            foreach (var throwValue in GroupedThrowsArray)
             {
                 Console.Write($"{throwValue}, ");
             }
 
             Console.WriteLine(" ]");
-            //foreach (var item in newArr)
+            //foreach (var item in GroupedThrowsArray)
             //{
             //    Console.WriteLine($"Item: {item}");
             //}
             //var frameNr = 1;
-            for (int i = 0, frameNumber = 1; i < newArr.Length && i + 1 < newArr.Length && frameNumber <= 10; i += 2, frameNumber++)
+            for (int i = 0, frameNumber = 1; i < GroupedThrowsArray.Length && i + 1 < GroupedThrowsArray.Length && frameNumber <= 10; i += 2, frameNumber++)
             {
                 Console.WriteLine($"Frame {frameNumber}");
-                Console.WriteLine($"Current item: [{i}]: {newArr[i]}");
-                Console.WriteLine($"Next item:[{i+1}]: {newArr[i+1]}");
-                if (i + 1 == newArr.Length) // extra throw situation >>> does it fall under frame = 11? and if so is this check even needed?
+                Console.WriteLine($"Current item: [{i}]: {GroupedThrowsArray[i]}");
+                Console.WriteLine($"Next item:[{i+1}]: {GroupedThrowsArray[i+1]}");
+                if (i + 1 == GroupedThrowsArray.Length) // extra throw situation >>> does it fall under frame = 11? and if so is this check even needed?
                 {
                     break;
                 }
                 else
                 {
-                    var firstThrow = Int32.Parse(newArr[i]); // do smth like static GetIntFromString(str)
-                    var secondThrow = Int32.Parse(newArr[i + 1]);
+                    var firstThrow = GroupedThrowsArray[i]; // do smth like static GetIntFromString(str)
+                    var secondThrow = GroupedThrowsArray[i + 1];
                     Console.WriteLine($"[{firstThrow}][{secondThrow}]");
 
                     if (firstThrow == 10) // TODO: case when next 2 vals are 10
                     {
                         Console.WriteLine("STRIKE");
-                        var next2Throws = GetNextTwoThrows(i, newArr);
-                        var nextThrowOne = Int32.Parse(next2Throws[0]);
-                        var nextThrowTwo = Int32.Parse(next2Throws[1]);
-                        if (nextThrowOne == 10)
-                        {
-                            nextThrowTwo = Int32.Parse(GetNextThrow(i + 2, newArr));
-                            Console.WriteLine(nextThrowTwo);
-                            var currScore = MaxScore + nextThrowOne + nextThrowTwo;
-                            Console.WriteLine($"Current SCORE: {currScore}");
-                            TotalScore += currScore;
-                            Console.WriteLine($"Total SCORE: {TotalScore}");
-                            continue;
-                        }
-                        else
-                        {
-                            Console.WriteLine($"EXTRA 2 THROWS: [{next2Throws[0]}][{next2Throws[1]}]");
-                            var currentScore = MaxScore + Int32.Parse(next2Throws[0]) + Int32.Parse(next2Throws[1]);
-                            Console.WriteLine($"Current score: {currentScore}");
-                            TotalScore += currentScore;
-                            Console.WriteLine($"Total score: {TotalScore}");
-                        }
+                        HandleStrike(GroupedThrowsArray, i);
                     }
                     else if ((firstThrow + secondThrow) == 10)
                     {
                         Console.WriteLine("SPARE");
-                        var nextThrow = Int32.Parse(GetNextThrow(i, newArr));
+                        var nextThrow = GetNextThrow(i, GroupedThrowsArray);
                         var currentScore = MaxScore + nextThrow;
                         Console.WriteLine($"Current score: {currentScore}");
                         TotalScore += currentScore;
@@ -92,85 +73,25 @@ namespace BowlingScore
                         TotalScore += currentScore;
                         Console.WriteLine($"Total score: {TotalScore}");
                     }
-
-                    //if ((firstThrow + secondThrow) == 10)
-                    //{
-                    //    Console.WriteLine("SPARE");
-                    //    var nextThrow = Int32.Parse(GetNextThrow(i));
-                    //    var currentScore = MaxScore + nextThrow;
-                    //    Console.WriteLine($"Current score: {currentScore}");
-                    //    Score += currentScore;
-                    //    Console.WriteLine($"Total score: {Score}");
-                    //}
-                    //else
-                    //{
-                    //    var currentScore = firstThrow + secondThrow;
-                    //    Console.WriteLine($"Current score: {currentScore}");
-                    //    Score += currentScore;
-                    //    Console.WriteLine($"Total score: {Score}");
-                    //}
-
                 }
             }
-
             return TotalScore;
-
-            //for (var i = 0; i < 20; i += 2)
-            //{
-
-            //    if (i + 1 >= ThrowArray.Length)
-            //    {
-            //        if (ThrowArray[i] == "10")
-            //        {
-            //            Console.WriteLine(ThrowArray[i]);
-            //            Console.WriteLine(0);
-            //            Console.WriteLine("---");
-            //            i--;
-            //            continue;
-            //        }
-            //        Console.WriteLine(ThrowArray[i]);
-            //        Console.WriteLine("---");
-            //        continue;
-            //    }
-            //    else
-            //    {
-            //        if (ThrowArray[i] == "10")
-            //        {
-            //            Console.WriteLine(ThrowArray[i]);
-            //            Console.WriteLine(0);
-            //            Console.WriteLine("---");
-            //            i--;
-            //            continue;
-            //        }
-            //        Console.WriteLine(ThrowArray[i]);
-            //        Console.WriteLine(ThrowArray[i + 1]);
-            //        Console.WriteLine("---");
-            //    }
-            //}
-            //var i = 0;
-            //while(i < ThrowArray.Length && i + 1 < ThrowArray.Length)
-            //var inputArray = _inputReader.GetThrowArray(_filePath);
-            //var i = 1;
-            //foreach (string score in inputArray)
-            //{
-            //    Console.WriteLine($"Lovitura {i++}: {score}");
-            //}
         }
 
-        private static string[] CreateNewArray(string[] oldArray) // Helps a bit
+        private static int[] ExtractScores(string[] throwsArray) // Helps a bit
         {
-            var newList = new List<string>();
-            foreach (var throwValue in oldArray)
+            var throwsValueList = new List<int>();
+            foreach (var throwValue in throwsArray)
             {
-                newList.Add(throwValue);
+                throwsValueList.Add(Int32.Parse(throwValue)); // add item to int array
                 if (throwValue == "10")
                 {
-                    newList.Add("0");
+                    throwsValueList.Add(0);
                 }
             }
 
-            var newArray = newList.ToArray();
-            return newArray;
+            return throwsValueList.ToArray();
+
         }
 
         private static int GetFrameScore(int throw1, int throw2)
@@ -182,29 +103,39 @@ namespace BowlingScore
             return throws[0] + throws[1];
         }
 
-        private string GetNextThrow(int currentIndex, string[] newArray)
+        private int GetNextThrow(int currentIndex, int[] GroupedThrowsArrayay)
         {
             // + 2 because we are at pair [i, i+1] and we want to get next throw, which is i+2
-            return newArray[currentIndex + 2];
+            return GroupedThrowsArrayay[currentIndex + 2];
         }
 
-        private string[] GetNextTwoThrows(int currentIndex, string[] newArray)
+        private int[] GetNextTwoThrows(int currentIndex, int[] GroupedThrowsArrayay)
         {
-            return new string[2] { newArray[currentIndex + 2], newArray[currentIndex + 3]}; // same as above
+            return new int[2] { GroupedThrowsArrayay[currentIndex + 2], GroupedThrowsArrayay[currentIndex + 3]}; // same as above
         }
 
-        //public void test2Throws(int index)
-        //{
-        //    //ThrowArray = _inputReader.GetThrowArray(_filePath);
-        //    var throwsArr = this.GetNextTwoThrows(index);
-        //    Console.Write("[ ");
-        //    foreach (var throwVal in throwsArr)
-        //    {
-        //        Console.Write(throwVal);
-        //        Console.Write(", ");
-        //    }
-
-        //    Console.WriteLine(" ]");
-        //}
+        private void HandleStrike(int[] throwsArray, int currentIndex)
+        {
+            var next2Throws = GetNextTwoThrows(currentIndex, throwsArray);
+            var nextThrowOne = next2Throws[0];
+            var nextThrowTwo = next2Throws[1];
+            if (nextThrowOne == 10)
+            {
+                nextThrowTwo = GetNextThrow(currentIndex + 2, GroupedThrowsArray);
+                Console.WriteLine(nextThrowTwo);
+                var currScore = MaxScore + nextThrowOne + nextThrowTwo;
+                Console.WriteLine($"Current SCORE: {currScore}");
+                TotalScore += currScore;
+                Console.WriteLine($"Total SCORE: {TotalScore}");
+            }
+            else
+            {
+                Console.WriteLine($"EXTRA 2 THROWS: [{next2Throws[0]}][{next2Throws[1]}]");
+                var currentScore = MaxScore + next2Throws[0] + next2Throws[1];
+                Console.WriteLine($"Current score: {currentScore}");
+                TotalScore += currentScore;
+                Console.WriteLine($"Total score: {TotalScore}");
+            }
+        }
     }
 }
